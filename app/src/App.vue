5d5b25d781a91bf74e1aff1c5d388dc23ce18fd0
Benjamin Yee <benyee1209@gmail.com>
2025-02-05 14:00:45 -0500

    s
    s

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div class = "header">
-<h1>Grocery Store Simulation 2</h1>
+<h1 :titleClass>Technology Store</h1>
 <h2 >Search through grocery store items and add them to your cart</h2>
   </div>
 
-  <div class = "cards">
+  <div :cardsClass = "cards">
 
-    
   </div>
 
+<div :cartClass>
+  <h2>Total price: </h2>
+</div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { techItems } from '../items';
+import { reactive, ref } from 'vue'
+
+//import { techItems } from '../items';
 
 
+const message = ref('asdas')
+
+
+console.log(message.value)
+message.value = 'Changed'
 
 
 </script>
 
-<style scoped>
-
+<style scoped> 
+.title{
+  color: rgb(255, 0, 0);
+}
 </style>
+
 
