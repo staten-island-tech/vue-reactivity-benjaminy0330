4ac9f8cdd4e551d930ac4db6e6545ee03c349dd3
Benjamin <benyee1209@gmail.com>
2025-02-09 21:07:13 -0500

    Revert "asd"
    This reverts commit 229ae43057fc5affc0179ec062d302fbc916b36f.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class = "header">
+<h1 :class = "titleClass">Technology Store</h1>
+<h2 >Search through grocery store items and add them to your cart</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div :cardsClass>
+    <itemCard
+    v-for = "techItem in techItems"
+    :key = "techItems.name"
+    :techItem = "techItem"
+    />
+  </div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+/*
+import {techItems} from `../items.js`;
+
+const message = ref(`cool kid`)
+
+
+console.log(message.value)
+message.value = 'The message has been successfully changed'
+
+*/
+</script>
+
+<style scoped> 
+.title{
+  color: rgb(255, 0, 0);
+  text-align: center;
 }
 </style>
+
+
